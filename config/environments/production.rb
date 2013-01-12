b82187e10c4c2bfa5a715dc98a686d2df0c8fe04
John Colvin <colvin.john@gmail.com>
2013-01-12 17:40:08 -0500

    Copied heroku's example a little too closely

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Example::Application.configure do
+Movieapi::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
